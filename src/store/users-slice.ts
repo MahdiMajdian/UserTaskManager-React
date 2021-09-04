5682c6cac2bcdfaff42a7c0e0b90d1e7000448ab
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-04 23:42:18 +0430

    redux toolkit configured
    users slice created

---
+++ src/store/users-slice.ts
@@ -0,0 +1,78 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+interface ITask {
+	taskId: number
+	taskName: string
+	taskIsDone: boolean
+}
+interface IUser {
+	name: string
+	username: string
+	email: string
+	tasks: ITask[]
+}
+
+interface IAdd {
+	name: string
+	username: string
+	email: string
+	task: ITask
+}
+interface IToggle {
+	username: string
+	taskId: number
+}
+const initialState: IUser[] = [
+	{
+		name: "Mahdi Majdian",
+		username: "mahdimajdian77",
+		email: "MajdianMahdi@gmail.com",
+		tasks: [
+			{
+				taskId: 0,
+				taskName: "finish task manager project",
+				taskIsDone: false,
+			},
+		],
+	},
+]
+
+const userSlice = createSlice({
+	name: "users",
+	initialState,
+	reducers: {
+		addTask(state, action: PayloadAction<IAdd>) {
+			const user = state.find(
+				(user) => user.username === action.payload.username
+			)
+			if (!user) {
+				state.push({
+					name: action.payload.name,
+					username: action.payload.username,
+					email: action.payload.email,
+					tasks: [action.payload.task],
+				})
+			} else {
+				user.tasks.push(action.payload.task)
+			}
+		},
+		toggleIsDone(state, action: PayloadAction<IToggle>) {
+			const user = state.find(
+				(user) => user.username === action.payload.username
+			)
+			const task = user!.tasks.find(
+				(task) => task.taskId === action.payload.taskId
+			)
+			task!.taskIsDone = !task!.taskIsDone
+		},
+		removeTask(state, action: PayloadAction<IToggle>) {
+			const user = state.find(
+				(user) => user.username === action.payload.username
+			)
+			user!.tasks = user!.tasks.filter(
+				(item) => item.taskId !== action.payload.taskId
+			)
+		},
+	},
+})
+export const userActions = userSlice.actions
+export default userSlice
