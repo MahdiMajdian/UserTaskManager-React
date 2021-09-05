c8f12f4b322bc69e44c69c0c45fdb1ea4546f85b
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-05 13:38:10 +0430

    userCard made dynamic with props

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 
-const UserCard = () => {
+interface IUserCard {
+	id: number
+	name: string
+	username: string
+	email: string
+}
+
+const UserCard: React.FC<IUserCard> = (props) => {
 	return (
-		<div className="rounded-lg p-4 w-full flex flex-col justify-between gap-8 bg-gradient-to-tr from-sky-400 to-purple-600">
+		<div className="rounded-lg p-4 w-full flex flex-col justify-between gap-8 bg-gradient-to-tr from-orange-300 via-orange-500 to-orange-600">
 			<div>
-				<h1 className="font-bold text-2xl text-white">Mahdi Majdian</h1>
-				<p className="text-gray-200 font-medium">(majdian_mahdi)</p>
+				<h1 className="font-bold text-2xl text-white">{props.name}</h1>
+				<p className="text-gray-200 font-medium">({props.username})</p>
 			</div>
 			<div className="flex justify-center items-center">
 				{/* <TiTick className="w-6 h-6 text-green-300" /> */}
