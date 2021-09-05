44d6fe5897509f3b9893e7021672c082c2ae39e1
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-05 09:25:34 +0430

    header section created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import React from "react"
 
 function App() {
-	return <div></div>
+	return (
+		<>
+			<header className="w-full flex items-center justify-between h-36 bg-gradient-to-tr from-sky-400 to-purple-600 ">
+				<h1 className="font-medium text-white text-3xl p-8">
+					Task Managment
+				</h1>
+				<div className="flex gap-16 px-16">
+					<div className="flex flex-col items-center gap-2">
+						<p className="text-white font-semibold text-xl">
+							Total
+						</p>
+						<div className="rounded-full w-16 h-16 flex items-center justify-center bg-gradient-to-tr from-sky-600 to-sky-300">
+							<p className="text-white font-bold text-3xl ">
+								125
+							</p>
+						</div>
+					</div>
+					<div className="flex flex-col items-center gap-2">
+						<p className="text-white font-semibold text-xl">
+							Completed
+						</p>
+						<div className="rounded-full w-16 h-16 flex items-center justify-center bg-gradient-to-tr from-sky-600 to-sky-300">
+							<p className="text-white font-bold text-3xl ">
+								125
+							</p>
+						</div>
+					</div>
+					<div className="flex flex-col items-center gap-2">
+						<p className="text-white font-semibold text-xl">
+							Remaining
+						</p>
+						<div className="rounded-full w-16 h-16 flex items-center justify-center bg-gradient-to-tr from-sky-600 to-sky-300">
+							<p className="text-white font-bold text-3xl ">
+								125
+							</p>
+						</div>
+					</div>
+				</div>
+			</header>
+		</>
+	)
 }
 
 export default App
