43ec4e9f397929ac151442c931d462b93ee27a7a
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-06 01:57:30 +0430

    added remaining tasks number on UserCard component

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { TiTick } from "react-icons/ti"
+import { useAppSelector } from "../../hooks/store-hook"
 
 interface IUserCard {
 	id: number
@@ -9,6 +11,13 @@ interface IUserCard {
 }
 
 const UserCard: React.FC<IUserCard> = (props) => {
+	const userTasks = useAppSelector((state) => state.tasks).filter(
+		(task) => task.userId === props.id
+	)
+	const remaining = userTasks.filter((item) => !item.completed).length
+	const detailText = remaining
+		? `${remaining} tasks Remaining ...`
+		: "All Tasks Completed"
 	return (
 		<div
 			className="rounded-lg p-4 w-full flex flex-col justify-between gap-8 bg-gradient-to-tr from-orange-300 via-orange-500 to-orange-600"
@@ -18,8 +27,8 @@ const UserCard: React.FC<IUserCard> = (props) => {
 				<p className="text-gray-200 font-medium">({props.username})</p>
 			</div>
 			<div className="flex justify-center items-center">
-				{/* <TiTick className="w-6 h-6 text-green-300" /> */}
-				<p className="text-white">12 tasks Remaining ...</p>
+				{!remaining && <TiTick className="w-6 h-6 text-green-300" />}
+				<p className="text-white">{detailText}</p>
 			</div>
 		</div>
 	)
